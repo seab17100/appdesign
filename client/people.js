9703c6feabd64a075a7a5398ff03262cb7cee981
tim <tjhickey@brandeis.edu>
2015-03-30 16:27:25 -0400

    added profile pages

--- client/people.js
+++ client/people.js
@@ -1,3 +1,11 @@
 Template.people.helpers({
 	profiles: function(){ return Profiles.find({});}
 })
+
+Template.people.events({
+	"click .profile-photo": function(event){
+		console.log(event.target);
+		console.log(this);
+		
+	}
+})
