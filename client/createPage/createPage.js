7ea4ab1255541dc7bbef99a2c28600cee146b4e5
Avi <avibenmnayor@gmail.com>
2015-07-20 15:23:10 -0400

    fixed broken text

--- client/createPage/createPage.js
+++ client/createPage/createPage.js
@@ -3,10 +3,3 @@
 
 //COOL BUT NEED TO ASSIGN TO SOMETHING	
 //confirm("Are you sure you would like to go back?")
-//if (vehicle= "student"){
-//	<a href="http://localhost:3000/studentProfilePage"></a>;
-//};
-
-//if (vehicle="student"){
-
-//}
