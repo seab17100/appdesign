32b82e18507d62197e8280ff19a9e482f3d576f8
Avi <avibenmnayor@gmail.com>
2015-07-21 09:34:39 -0400

    Pre-Login Page

--- client/tutorlist.js
+++
@@ -1,9 +0,0 @@
-Template.tutorlist.helpers({
-	tutors: function(){return Profiles.find({},{sort:{createdAt:-1}});}
-})
-
-Template.tutorRow.events({
-	"click button.delete": function(event){
-		Profiles.remove(this._id);
-	}
-})
